05e64b1ba6acba0b20050ffeb1029e9d665a3092
Cornelius Roemer <cornelius.roemer@gmail.com>
2022-01-04 23:19:22 +0100

    feat: add incidences as extra predictors

--- scripts/plz_to_landkreisID.py
+++ scripts/plz_to_landkreisID.py
@@ -14,6 +14,10 @@ seq = pd.read_csv("data/meta_lineages.csv", sep=",", dtype=str)
 seq.dropna(subset=["SENDING_LAB_PC"], inplace=True)
 sending = seq.SENDING_LAB_PC.unique()
 plzs_to_impute = sending[~np.isin(sending,df.zipcode.unique())]
+plzs_to_impute
+#%%
+# df[df.zipcode == '17475']
+# np.isin(sending,'17475')
 #%%
 def nearest_neighbour_plz(df, plz):
     # Find nearest neighbours
@@ -28,7 +32,7 @@ nearest_neighbour_plz(df, '01000')
 imputed_zips = pd.DataFrame({'zipcode': plzs_to_impute})
 imputed_zips['imputed'] = imputed_zips.zipcode.apply(lambda x: nearest_neighbour_plz(df,x))
 
-imputed_zips_joined = imputed_zips.join(df.set_index('zipcode'), on='imputed').drop('zipcode',axis=1).rename(columns={'imputed': 'zipcode'})
+imputed_zips_joined = imputed_zips.join(df.set_index('zipcode'), on='imputed').drop({'imputed'}, axis=1)
 df = df.append(imputed_zips_joined).reset_index(drop=True)
 #%%
 # Restrict to zips that appear in the data
@@ -41,6 +45,9 @@ incidence[incidence.IdMeldeLandkreis.isin(df.zipcode.unique())]
 df[~df.community_code.isin(incidence.IdMeldeLandkreis.unique())]
 # %%
 df['inc_2021-11-25'] = df.community_code.apply(lambda x: incidence.loc[incidence.IdMeldeLandkreis == x, '2021-11-25'].values[0])
+df['inc_2021-12-10'] = df.community_code.apply(lambda x: incidence.loc[incidence.IdMeldeLandkreis == x, '2021-12-10'].values[0])
 df['inc_2021-12-20'] = df.community_code.apply(lambda x: incidence.loc[incidence.IdMeldeLandkreis == x, '2021-12-20'].values[0])
 df.to_csv("data/plz_metadata.csv", index=False)
+# %%
+
 # %%
